561f2ff4f85e2fe37c0f5d11a07f6217a93f1cf3
Ars <shiro1702@gmail.com>
2021-01-13 13:08:09 +0700

    feat: new trade

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,16 @@
 <style lang="sass">
 #app
   color: #2c3e50
-
 .w-25
   width: 25%
 .w-50
   width: 50%
 .w-100
   width: 100%
-
 .field_bodered
   padding: 8px
   border-radius: 8px
   border: 1px solid rgba(10, 10, 10, 0.05)
-
 .a-select
   .vs__dropdown-toggle
     height: 100%
